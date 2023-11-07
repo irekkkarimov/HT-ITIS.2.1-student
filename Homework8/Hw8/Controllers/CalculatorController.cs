06f3efb501e2eb1100d0608f8f5d4e46c964f411
irekkkarimov <irekkkarimov@mail.ru>
2023-11-07 20:33:31 +0300

    calculator works, tests passed

--- Homework8/Hw8/Controllers/CalculatorController.cs
+++ Homework8/Hw8/Controllers/CalculatorController.cs
@@ -12,7 +12,17 @@ public class CalculatorController : Controller
         string operation,
         string val2)
     {
-        throw new NotImplementedException();
+        var operationTuple = ValidateOperation(operation);
+        var (ifArg1Parsed, ifArg2Parsed) =
+            (double.TryParse(val1, out var arg1Parsed), double.TryParse(val2, out var arg2Parsed));
+
+        if (!operationTuple.Item1)
+            return BadRequest(Messages.InvalidOperationMessage);
+        if (!ifArg1Parsed || !ifArg2Parsed)
+            return BadRequest(Messages.InvalidNumberMessage);
+        return arg2Parsed == 0 
+            ? BadRequest(Messages.DivisionByZeroMessage)
+            : Ok(CalculateSwitchOperation(calculator, arg1Parsed, operationTuple.Item2, arg2Parsed).ToString(CultureInfo.CurrentCulture));
     }
     
     [ExcludeFromCodeCoverage]
@@ -22,4 +32,25 @@ public class CalculatorController : Controller
             "Заполните val1, operation(plus, minus, multiply, divide) и val2 здесь '/calculator/calculate?val1= &operation= &val2= '\n" +
             "и добавьте её в адресную строку.");
     }
+    
+    [NonAction]
+    private (bool, Operation) ValidateOperation(string operation) =>
+        operation switch
+        {
+            "Plus" => (true, Operation.Plus),
+            "Minus" => (true, Operation.Minus),
+            "Multiply" => (true, Operation.Multiply),
+            "Divide" => (true, Operation.Divide),
+            _ => (false, Operation.Invalid)
+        };
+    
+    [NonAction]
+    private double CalculateSwitchOperation(ICalculator calculator, double arg1, Operation operation, double arg2) =>
+        operation switch
+        {
+            Operation.Plus => calculator.Plus(arg1, arg2),
+            Operation.Minus => calculator.Minus(arg1, arg2),
+            Operation.Multiply => calculator.Multiply(arg1, arg2),
+            Operation.Divide => calculator.Divide(arg1, arg2)
+        };
 }
